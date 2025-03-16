1002744f21c79fdf1aa873f93a1a3f3b26438044
piyush45 <singh45piyush@gmail.com>
2025-03-16 18:06:35 +0530

    image issues

--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,12 @@
 
 export const getImageUrl = (path) => {
   // Construct the absolute path to the assets_ folder
-  const baseUrl = window.location.origin; // Get the base URL (e.g., http://localhost:5173)
+
+  // Get the base URL (e.g., http://localhost:5173)
+  const baseUrl = window.location.origin; 
   const url = `${baseUrl}/assets_/${path}`;
-  console.log("Path Argument:", path); // Debugging
-  console.log("Resolved URL:", url); // Debugging
+  console.log("Path Argument:", path); 
+  console.log("Resolved URL:", url); 
   return url;
 
   // return `/assets_/${path}`;
