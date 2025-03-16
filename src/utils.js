9ad1e42b4e9fcccb7d952bd5611a8051649c751f
piyush45 <singh45piyush@gmail.com>
2025-03-16 18:16:57 +0530

    Fix image paths for production

--- src/utils.js
+++ src/utils.js
@@ -3,12 +3,12 @@ export const getImageUrl = (path) => {
   // Construct the absolute path to the assets_ folder
 
   // Get the base URL (e.g., http://localhost:5173)
-  const baseUrl = window.location.origin; 
-  const url = `${baseUrl}/assets_/${path}`;
+  // const baseUrl = window.location.origin; 
+  // const url = `${baseUrl}/assets_/${path}`;
   console.log("Path Argument:", path); 
-  console.log("Resolved URL:", url); 
-  return url;
+  // console.log("Resolved URL:", url); 
+  // return url;
 
-  // return `/assets_/${path}`;
+  return `/assets_/${path}`;
 
 };
