e0eeee59b79d16f76b6e73d7de6f17f42da1b9d2
piyush45 <singh45piyush@gmail.com>
2025-04-13 13:34:33 +0530

    navbar

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,12 @@
 
+export const getImageUrl = (path) => {
+  // Construct the path relative to the public folder
+  const url = `/assets_/${path}`;
+  console.log("Path Argument:", path); // Debugging
+  console.log("Resolved URL:", url); // Debugging
+  return url;
+};
+
 // export const getImageUrl = (path) => {
   // Construct the absolute path to the assets_ folder
 
@@ -16,10 +24,40 @@
 //   return new URL(`../assets_/${path}`, import.meta.url).href;
 // };
 
-export const getImageUrl = (path) => {
-  // Construct the path relative to the public folder
-  const url = `/assets_/${path}`;
-  console.log("Path Argument:", path); // Debugging
-  console.log("Resolved URL:", url); // Debugging
-  return url;
-};
+
+/**
+git init 
+git add .
+git conmmit -m ""
+git push origin main
+
+git show 
+
+git diff
+git diff --staged 
+git diff "path"
+
+git log
+git log commit_id
+git log filename
+git log -2
+git log -p
+git log --follow "path"
+
+git log flags
+--oneline
+--decorator
+--graph
+--grep=<pattern>
+
+HEAD 
+Symbols -> ~ ^ 
+
+commit and delete commit
+view history commit 
+
+intilizing repo, pushing, pulling, clone repo
+commiting, undo commit, viewing commits history
+creating, merging, viewing and handling conflict in branches
+
+ */
